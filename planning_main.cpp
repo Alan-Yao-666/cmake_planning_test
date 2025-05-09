ebcbe9ab75603af9c4124b6eac818bfb6c4e2a8e
junhua-yao <13727289085@163.com>
2025-05-09 22:04:38 +0800

    version 0.0.1

---
+++ planning_main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"process.h"
+using std::cout,std::endl;
+int main()
+{
+    cout <<"Planning start"<<endl;
+    Process pro;
+    pro.plan_process();
+    cout <<"planning end"<<endl;
+    return 0;
+}
